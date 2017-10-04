5fb4ae85520b94c2e95b041fadfbbcc936a3c03d
xdass <420600@gmail.com>
2017-10-04 15:51:16 +0500

    Fix sqrt

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,15 @@
 from math import sqrt
-
+import cmath
 
 def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
-        return root1, None
-    else:
-        return root1, root2
+	discriminant = b ** 2 - 4 * a * c
+	root1 = (-b - cmath.sqrt(discriminant)) / (2 * a)
+	root2 = (-b + cmath.sqrt(discriminant)) / (2 * a)
+
+
+	if discriminant == 0:
+		return root1, None
+	elif discriminant > 0 :
+		return root1, root2
+	else:
+		return None, None
