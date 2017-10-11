baa9bab62771b0a69dbce5fc8ce6851d9b91e441
Dmitry <420600@gmail.com>
2017-10-11 12:21:54 +0500

    Update quadratic_equation.py
    Add bracets to formula

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -7,7 +7,7 @@ def get_roots(a, b, c):
     if discriminant < 0:
         return None, None
     elif discriminant == 0:
-        root1 = - b / (2 * a)
+        root1 = - (b / (2 * a))
         return root1, None
     else:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
