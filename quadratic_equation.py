8921105e8817fa6527c2fd4eb395e3747ce92ee7
Dmitry <420600@gmail.com>
2017-10-11 08:23:14 +0500

    Update quadratic_equation.py
    Change IF statements for calculating roots

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -4,12 +4,12 @@ from math import sqrt
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
 
-    if discriminant == 0:
+    if discriminant < 0:
+        return None, None
+    elif discriminant == 0:
         root1 = - b / (2 * a)
         return root1, None
-    elif discriminant > 0:
+    else:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
-    else:
-        return None, None
