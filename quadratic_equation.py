b0da0898422877f2a38ecbb7095247e748b2c3d9
Dmitry <420600@gmail.com>
2017-10-11 13:02:30 +0500

    Update quadratic_equation.py
    Improve logic, exclude duplicate computation

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,12 +3,8 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-
     if discriminant < 0:
         return None, None
-    elif discriminant == 0:
-        root1 = - (b / (2 * a))
-        return root1, None
     else:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
